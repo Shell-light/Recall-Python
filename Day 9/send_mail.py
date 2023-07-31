0889a4656144aef7c4b18424a72c069f8feb5707
neon1 <ulightshell@gmail.com>
2023-07-31 15:55:46 +0500

    Send message email

---
+++ Day 9/send_mail.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart 
+
+# environment variables
+username = 'muhammaduktamoof@gmail.com'
+password = '1305131016022906'
+
+def send_mail(text='Email Body', subject='Hello World', from_email='Hungry Py <muhammaduktamoof@gmail.com>', to_emails=None, html=None):
+    assert isinstance(to_emails, list)
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_emails)
+    msg['Subject'] = subject
+
+    txt_part = MIMEText(text, 'plain')
+    msg.attach(txt_part)
+    if html != None:
+        html_part = MIMEText(html, 'html')
+        msg.attach(html_part)
+    msg_str = msg.as_string()
+
+    # login to my smtpserver
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email, to_emails, msg_str)
+    server.quit()    
