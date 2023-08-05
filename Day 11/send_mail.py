5a65bc4a58480b2c15aaab0a9ce3805324ccda2c
neon1 <ulightshell@gmail.com>
2023-08-05 18:57:35 +0500

    Classes and state

--- Day 11/send_mail.py
+++ Day 11/send_mail.py
@@ -2,39 +2,65 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart 
 
+from templates import Template
 # environment variables
 username = ''
 password = ''
 
 class Emailer():
     subject = ""
+    context = {}
     to_emails = []
+    has_html = False
+    test_send = False
     from_email='Hungry Py <muhammaduktamoof@gmail.com>'
-    def __init__(self, text, subject="", template_name=None, context={}, template_html=None, to_emails=None):
+    template_html = None
+    template_name = None
+
+    def __init__(self, text, subject="", template_name=None, context={}, template_html=None, to_emails=None, test_send=False):
         if template_name == None and template_html == None:
             raise Exception ("You must set a template")
         assert isinstance(to_emails, list)
         self.to_emails = to_emails
         self.subject = subject
+        if template_html != None:
+            self.has_html = True
+            self.template_html = template_html
+        self.template_name = template_name
+        self.context = context
+        self.test_send = test_send
 
-    def send_mail(text='Email Body', subject='Hello World', to_emails=None, html=None):
-        
+    def format_msg(self):
         msg = MIMEMultipart('alternative')
-        msg['From'] = from_email
-        msg['To'] = ", ".join(to_emails)
-        msg['Subject'] = subject
+        msg['From'] = self.from_email
+        msg['To'] = ", ".join(self.to_emails)
+        msg['Subject'] = self.subject
 
-        txt_part = MIMEText(text, 'plain')
-        msg.attach(txt_part)
-        if html != None:
-            html_part = MIMEText(html, 'html')
+        if self.template_name != None:
+            tmpl_str = Template(template_name=self.template_name, context=self.context)
+            txt_part = MIMEText(tmpl_str.render(), 'plain')
+            print(txt_part)
+            msg.attach(txt_part)
+        if self.template_html != None:
+            tmpl_str = Template(template_name=self.template_html, context=self.context)
+            html_part = MIMEText(tmpl_str.render(), 'html')
+            print(html_part)
             msg.attach(html_part)
         msg_str = msg.as_string()
-
+        return msg_str
+    
+    def send(self):
+        msg = self.format_msg()
         # login to my smtpserver
-        server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-        server.ehlo()
-        server.starttls()
-        server.login(username, password)
-        server.sendmail(from_email, to_emails, msg_str)
-        server.quit()    
+        did_send = False
+        if not self.test_send:
+            with smtplib.SMTP(host='smtp.gmail.com', port=587) as server:
+                server.ehlo()
+                server.starttls()
+                server.login(username, password)
+                try:
+                    server.sendmail(from_email, to_emails, msg_str)
+                    did_send = True
+                except:
+                    did_send = False
+        return did_send
